a52288c0c29168b15a7a859f9643e845954525d7
NoThrottle <ntyt.em@gmail.com>
2021-12-28 12:48:00 +0800

    updated naming

---
+++ backup/modfirm/src/installer/UpdateManager.java
@@ -0,0 +1,264 @@
+package installer;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+import installer.networking.HttpRequestLiveTest;
+import net.harawata.appdirs.AppDirs;
+import net.harawata.appdirs.AppDirsFactory;
+
+
+public class UpdateManager {
+
+	
+	//Debug Only----
+	public static void main(String[] args) {
+
+		System.out.println(GetClientModPath(".mc"));
+		System.out.println(ClientVersion("readable"));
+		
+		
+	}
+	//Debug only-----
+	
+	
+	
+	
+	//-----------PUBLIC-RETURNS------------
+	public static String ClientVersion(String type) {
+		return ProcessClientVersion(type);
+	}
+	public static String ServerVersion(String type) {
+
+		return GetServerVersion(type);
+	}	
+	public static Boolean IsRunninginMods() {
+		return CorrectInstallation();
+	}
+	public static String IntegrityCheck() {
+		
+		return APIKey;
+	}
+	
+	
+	
+	
+	//Statics for mod updates
+	static Path ClientModPath = GetClientModPath("mods");
+	static Path ClientMCPath = GetClientModPath(".mc");
+	
+	
+	
+	//-------------PROCESSING-------------
+	//Complete
+	private static String ProcessClientVersion(String type) {
+		
+		String version = "Unknown";
+		String versioncode = "Unknown";
+		String readable = "Unknown";
+		
+		//Check if the directory actually exists, else return error
+		File f = new File(ClientModPath.toString());
+		if (!f.isDirectory()) {
+		   version = "Error 1"; //You haven't launched Minecraft with fabric installed.
+			File g = new File(ClientMCPath.toString());
+			if (!g.isDirectory()) {
+				version = version + "A"; //Just improves the error depth, not an actual version. You don't have Minecraft Installed
+				return version;
+			} else {return version;}
+		}
+		
+		
+		//Check if version.txt exists. If yes, parse that instead, if no, continue		
+		 try {
+		      File myObj = new File(ClientModPath.toString() + "/version.txt");
+		      try (Scanner myReader = new Scanner(myObj)) {
+				while (myReader.hasNextLine()) {
+					
+					if (readable == "Unknown") {
+					    readable = myReader.nextLine();
+					} else {
+						versioncode = myReader.nextLine();
+					}
+				    System.out.println(version + " " + versioncode); //logs
+				  }
+				  myReader.close();
+			}
+		    } catch (FileNotFoundException e) {
+		      System.out.println("Version File not found."); //logs
+		      version = "v1 for Patch 01"; //First version did not have version file.		
+		      versioncode = "1";
+		    }
+			
+			if (type == "readable") { //returns v1 for Patch 01
+				return readable;
+			} else if (type == "coded") {//returns an integer(as string)
+				return versioncode;
+			}
+			
+		 
+		return "Unknown Error";
+	}
+	
+	
+//	private static Boolean VerifyCompleteMods() {
+//		
+//		
+//		listFilesForFolder(ClientModPath.toFile()); //make to variable
+//		GetServerModList();
+//		//then compare to json from server
+//				
+//	      //compare
+//	      if (VerifyCompleteMods() == true) {
+//	    	  //log that everything is fine and complete
+//	    	  return version;
+//	      } else {
+//	    	  version = "Missing Files";
+//	    	  return version;
+//	    	  //fetch missing mods list and ask the user if they want to update
+//	      }
+//	
+//	      
+//	      
+//	      
+//		return true;
+//		
+//	}
+	
+	//Complete
+	private static String GetServerVersion(String type) {
+		
+		String version = "Fetching Version...";
+		
+		if (type == "readable") { //returns v1 for Patch 01
+			try {
+				version = ValueFromSheet("F3:F3").toString();
+				version = version.substring(2, version.length()-2);
+			} catch (IOException e) {
+				e.printStackTrace();
+				//logs
+				version = "Error";
+			}
+		} else if (type == "coded") {//returns an integer(as string)
+			try {
+				version = ValueFromSheet("E3:E3").toString();
+				version = version.substring(2, version.length()-2);
+			} catch (IOException e) {
+				e.printStackTrace();
+				//logs
+				version = "Error";
+			}
+		}
+		
+
+		return version;	
+	}
+	
+	//Complete
+	private static Boolean CorrectInstallation() {
+		
+		Path runpath = null;
+		Path modpath = GetClientModPath("mods");
+		Boolean iscorrect = null;
+		
+		try {
+			File runpathfile = new File(UpdateManager.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+			runpath = runpathfile.toPath();
+		} catch (URISyntaxException e) {
+			// Log any errors
+			e.printStackTrace();
+		}
+		
+////		Debugging:
+//		System.out.println(modpath);
+//		System.out.println(runpath);
+		
+		if (modpath != runpath){
+			iscorrect = false;
+		} else {
+			iscorrect = true;
+		}
+		
+		return iscorrect;
+	}
+	
+	
+	
+	//------------PRIVATE RETURNS-------------
+	
+	//Complete
+	private static Path GetClientModPath(String what) {
+		
+		AppDirs appDirs = AppDirsFactory.getInstance();
+		
+		if (what == "mods") {
+			Path path = Paths.get(appDirs.getUserDataDir(".minecraft", "mods", null, true));
+			return path;
+		} else if (what == ".mc") {
+			Path path = Paths.get(appDirs.getUserDataDir(".minecraft", null, null, true));
+			return path;
+		} else {
+			return null;
+		}
+
+		//send process to log
+	}
+	
+	//Complete
+	private static List<String> listFilesForFolder(final File folder) {
+		
+		File[] listOfFiles = folder.listFiles();
+		List<String> files = new ArrayList<String>();
+
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		    	files.add(file.getName().toString());
+//		        System.out.println(files);
+		    }
+		}
+		return files;
+	}
+	
+	//Complete (logs and debug)
+    private static String SPREADSHEET_ID = "1e7FJRyB1rG7yctlLHcqf3nP0-CN4aaMbN2x2oIjvZSM";
+	private static String APIKey = "AIzaSyAFGJFSJfQQn0FdtTkz63_KINBCTLQ_Ikg";
+	private static List ValueFromSheet(String range) throws IOException {
+		
+		List data = HttpRequestLiveTest.Request(SPREADSHEET_ID, range, APIKey);	
+		System.out.println(data);//debug
+		return data;
+		
+	}
+	
+	
+	private static List<String> GetServerModList() {
+		
+		String fileId = "0BwwA4oUTeiV1UVNwOHItT0xfa2M";
+		OutputStream outputStream = new ByteArrayOutputStream();
+//		driveService.files().get(fileId)
+//		    .executeMediaAndDownloadTo(outputStream);
+
+		
+		
+		return null;
+	}
+
+}
---
+++ backup/modfirm/src/installer/networking/HttpRequestLiveTest.java
@@ -0,0 +1,42 @@
+package installer.networking;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HttpRequestLiveTest {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * @param sheetid
+     * @param range
+     * @param key
+     * @return
+     * @throws IOException
+     */
+    public static List Request(String sheetid, String range, String key) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+            .url("https://sheets.googleapis.com/v4/spreadsheets/"  + sheetid + "/values/" + range + "?key=" + key)
+            .build(); // defaults to GET
+
+        Response response = client.newCall(request).execute();
+
+        APOD apod = mapper.readValue(response.body().byteStream(), APOD.class);
+
+        System.out.println("https://sheets.googleapis.com/v4/spreadsheets/"  + sheetid + "/values/" + range + "?key=" + key);
+        System.out.println(apod.values);
+        
+        return apod.values;
+
+    }
+
+}
+
