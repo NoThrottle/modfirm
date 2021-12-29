c5a4f7db05839e48d3b648263e80304acee1c365
NoThrottle <ntyt.em@gmail.com>
2021-12-30 02:05:54 +0800

    completed yaml configuration

--- modfirm/src/installer/ParseConfig.java
+++ modfirm/src/installer/ParseConfig.java
@@ -5,6 +5,12 @@ import java.util.List;
 public class ParseConfig {
     private String apiKey;
     private String sheetID;
+    private String versionOld_coded;
+    private String versionOld_readable;
+    private String versionNew_coded;
+    private String versionNew_readable;
+    private String changelog;
+    private String modsListRange;
 
     public ParseConfig() {
 
@@ -12,14 +18,28 @@ public class ParseConfig {
 
     public ParseConfig(
     		String apiKey,
-    		String sheetID
+    		String sheetID,
+    		String versionOld_coded,
+    		String versionOld_readable,
+    		String versionNew_coded,
+    		String versionNew_readable,
+    		String changelog,
+    		String modsListRange
     		) {
     	
         super();
         this.apiKey = apiKey;
         this.sheetID = sheetID;
+        this.versionOld_coded = versionOld_coded;
+        this.versionOld_readable = versionOld_readable;
+        this.versionNew_coded = versionNew_coded;
+        this.versionNew_readable = versionNew_readable;
+        this.changelog = changelog;
+        this.modsListRange = modsListRange;
     }
 
+    
+    //-----PUBLIC_RETURNS------
     public String getApiKey() {
         return apiKey;
     }
@@ -27,8 +47,33 @@ public class ParseConfig {
     public String getSheetID() {
         return sheetID;
     }
+    
+    public String getVersionOld_coded() {
+    	return versionOld_coded;
+    }
 
+    public String getVersionOld_readable() {
+    	return versionOld_readable;
+    }
+    
+    public String getVersionNew_coded() {
+    	return versionNew_coded;
+    }
 
+    public String getVersionNew_readable() {
+    	return versionNew_readable;
+    }
+    
+    public String getChangelog() {
+    	return changelog;
+    }
+    
+    public String getModsListRange() {
+    	return modsListRange;
+    }
+    
+
+    
     //IDK what this does
     @Override
     public String toString() {
